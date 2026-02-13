99f09556e26b4e0d643d22ea3b4da08323ec228a
ashi <hazyashi@proton.me>
2026-02-13 01:44:38 -0800

    Correct text splitting and format timestamp
    Fix newline character in text splitting and update timestamp format.

--- txt2html.py
+++ txt2html.py
@@ -2,15 +2,15 @@ with open ('blog.txt', 'r') as file: text = file.read()
 
 html_text = '<html>\n<head>\n<title>Blog</title>\n<link rel="stylesheet" href="https://ashi.nekoweb.org/style.css">\n</head><body> \n <div class="nav-header"><nav>\n<a href="https://ashi.nekoweb.org/index.html">/home/</a>\n<a href="https://ashi.nekoweb.org/home/about.html">/about/</a>\n<a href="https://ashi.nekoweb.org/home/blog.html">/blog/</a>\n<a href="https://ashi.nekoweb.org/home/contact.html">/contact/</a>\n<a href="https://ashi.nekoweb.org/home/games.html">/games/</a></nav></div>\n<div class="header"><br><p>\n<span style="color: #eb6f92;"> ~/home/blog/posts</span></p></div>\n<div class="blog-post">\n'
 
-for line in text.split('n'): 
+for line in text.split('\n'): 
     html_text += f'<p>{line}</p>\n'
 
-html_text +='</div>\n</body>\n</html>'
+html_text +='<br><a href="https://ashi.nekoweb.org/home/blog.html">return-</a></div>\n</body>\n</html>'
 
 import time
 
 base_filename = 'blog.html'
-timestamp = time.strftime('%Y|M%m|D%d|H%H')
+timestamp = time.strftime('%Y_M%m_D%d_H%H')
 new_filename = f"{base_filename.rsplit('.', 1)[0]}_{timestamp}.{base_filename.rsplit('.', 1)[-1]}"
 
 with open(new_filename, "w") as file:
