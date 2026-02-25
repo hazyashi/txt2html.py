5a40aea43ac09edb7ae344a55ee089e932373d6b
ashi <hazyashi@proton.me>
2026-02-24 20:01:56 -0800

    Update txt2html.py

--- txt2html.py
+++ txt2html.py
@@ -1,6 +1,6 @@
 with open ('blog.txt', 'r') as file: text = file.read()
 
-html_text = '<html>\n<head>\n<title>Blog</title>\n<link rel="stylesheet" href="https://ashi.nekoweb.org/style.css">\n</head><body> \n <div class="nav-header"><nav>\n<a href="https://ashi.nekoweb.org/index.html">/home/</a>\n<a href="https://ashi.nekoweb.org/home/about.html">/about/</a>\n<a href="https://ashi.nekoweb.org/home/blog.html">/blog/</a>\n<a href="https://ashi.nekoweb.org/home/contact.html">/contact/</a>\n<a href="https://ashi.nekoweb.org/home/games.html">/games/</a></nav></div>\n<div class="header"><br><p>\n<span style="color: #eb6f92;"> ~/home/blog/posts</span></p></div>\n<div class="blog-post">\n'
+html_text = '<html>\n<head>\n<title>Blog</title>\n<link rel="stylesheet" href="https://ashi.nekoweb.org/style.css">\n</head><body>\n<div class="blog-post">\n'
 
 for line in text.split('\n'): 
     html_text += f'<p>{line}</p>\n'
